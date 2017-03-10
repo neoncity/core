89058e1e388c27f4b688b72de1161f8042db41d3
Horia Coman <horia141@gmail.com>
2017-03-10 18:09:37 +0200

    Add the share table.

---
+++ migrations/0004.create-share.js
@@ -0,0 +1,15 @@
+exports.up = (knex, Promise) => knex.schema.raw(`
+    CREATE TABLE core.share (
+        id Serial,
+        time_created Timestamp NOT NULL,
+        cause_id Int REFERENCES core.cause(id),
+        user_id Int NOT NULL
+    );
+
+    CREATE INDEX share_user_id ON core.donation(share_id);
+`);
+
+exports.down = (knex, Promise) => knex.schema.raw(`
+    DROP INDEX IF EXISTS core.share_user_id;
+    DROP TABLE IF EXISTS core.share;
+`);
