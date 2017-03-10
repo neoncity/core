7de5da0eed7f2c1e5408ea88eb22b4fd33412970
Horia Coman <horia141@gmail.com>
2017-03-10 16:32:59 +0200

    Add a cause table.

---
+++ migrations/0002.create-cause.js
@@ -0,0 +1,25 @@
+exports.up = (knex, Promise) => knex.schema.raw(`
+    CREATE TYPE CauseState AS ENUM ('active', 'succeeded', 'removed');
+
+    CREATE TABLE core.cause (
+        id Serial,
+        state CauseStatus,
+        user_id int NOT NULL,
+        time_created Timestamp NOT NULL,
+        time_last_updated Timestamp NOT NULL,
+        time_removed Timestamp NOT NULL,
+        title VarChar(128) NOT NULL,
+        description Text NOT NULL,
+        pictures Json NOT NULL,
+        deadline Timestamp NOT NULL,
+        goal Json NOT NULL
+    );
+
+    CREATE UNIQUE INDEX cause_user_id ON core.cause(user_id);
+`);
+
+exports.down = (knex, Promise) => knex.schema.raw(`
+    DROP INDEX IF EXISTS core.cause_user_id;
+    DROP TABLE IF EXISTS core.cause;
+    DROP TYPE CauseState
+`);
