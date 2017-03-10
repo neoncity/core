9db9389bc4d7cbf9bdee72da66852b712c7e18fb
Horia Coman <horia141@gmail.com>
2017-03-10 22:40:12 +0200

    New stuff.

--- migrations/0003.create-donation.js
+++ migrations/0003.create-donation.js
@@ -7,10 +7,12 @@ exports.up = (knex, Promise) => knex.schema.raw(`
         amount Json NOT NULL
     );
 
+    CREATE INDEX donation_cause_id ON core.donation(cause_id);
     CREATE INDEX donation_user_id ON core.donation(user_id);
 `);
 
 exports.down = (knex, Promise) => knex.schema.raw(`
+    DROP INDEX IF EXISTS core.donation_cause_id;
     DROP INDEX IF EXISTS core.donation_user_id;
     DROP TABLE IF EXISTS core.donation;
 `);
