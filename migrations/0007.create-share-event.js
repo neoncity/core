3c3fb3ea0149672a91b2b2e2f7488c1474cb64e9
Horia Coman <horia141@gmail.com>
2017-05-02 19:52:41 +0300

    Add share_event table

---
+++ migrations/0007.create-share-event.js
@@ -0,0 +1,20 @@
+exports.up = (knex, Promise) => knex.schema.raw(`
+    CREATE TABLE core.share_event (
+        -- Primary key
+	id Serial,
+	PRIMARY KEY (id),
+        -- Core properties
+        type SmallInt NOT NULL,
+        timestamp Timestamp NOT NULL,
+        data Jsonb NOT NULL,
+        -- Foreign key
+        share_id Int NOT NULL REFERENCES core.share(id)
+    );
+
+    CREATE INDEX share_event_share_id ON core.share_event(share_id);
+`);
+
+exports.down = (knex, Promise) => knex.schema.raw(`
+    DROP INDEX IF EXISTS core.share_event_share_id;
+    DROP TABLE IF EXISTS core.share_event;
+`);
