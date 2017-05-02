5b91ae03d855d97b39e79a3da885337697c09499
Horia Coman <horia141@gmail.com>
2017-05-02 19:48:15 +0300

    Add the cause_event table

---
+++ migrations/0005.create-cause-event.js
@@ -0,0 +1,21 @@
+exports.up = (knex, Promise) => knex.schema.raw(`
+    CREATE TABLE core.cause_event (
+        -- Primary key
+	id Serial,
+	PRIMARY KEY (id),
+        -- Core properties
+        type SmallInt NOT NULL,
+        timestamp Timestamp NOT NULL,
+        data Jsonb NOT NULL,
+        -- Foreign key
+        cause_id Int NOT NULL REFERENCES core.cause(id)
+    );
+
+    CREATE INDEX cause_event_user_id ON core.cause_event(cause_id);
+`);
+
+exports.down = (knex, Promise) => knex.schema.raw(`
+    DROP INDEX IF EXISTS core.cause_event_cause_id;
+    DROP TABLE IF EXISTS core.cause_event;
+`);
+
